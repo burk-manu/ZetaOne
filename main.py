501f8017034165cf9b67766e834a9ecedfd1de47
Manuel Bürki <burk.manu.2022@ksz.edu-zg.ch>
2025-03-31 07:36:12 +0200

    fixing input problems

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-# main application; creating GUI and habdles user inputs
+# main application; creating GUI and handles user inputs
 class CalculatorApp:
 
     def __init__(self, root):
@@ -11,9 +11,13 @@ class CalculatorApp:
 
         self.current_input = ""
 
-        self.entry = tk.Entry(root, width=14, font=("Helvetica Neue", 30, "bold"), borderwidth=1, relief="solid",)
+        self.entry = tk.Entry(root, validate="key",
+                              width=14, font=("Helvetica Neue", 30, "bold"), 
+                              borderwidth=1, relief="solid", justify="right")
         self.entry.configure(bg="#595959", fg="#C04F15")
         self.entry.grid(row=0, column=0, columnspan=4)
+
+        self.root.bind("<Key>", self.on_key_press)
 
         # buttons for the calculator
         buttons = [
@@ -35,54 +39,47 @@ class CalculatorApp:
             if col_val > 3:
                 col_val = 0
                 row_val += 1
-        
-        # binds the key press event to the entry
-        self.root.bind("<Key>", self.key_pressed)
 
-    # calculates the result of the user input and actualizes the entry
+    def on_key_press(self, event) -> None:
+        char = event.char
+        if char == "\r":
+            self.calculate_result()
+        elif char == "\b":
+            self.backspace()
+        elif char.isdigit() or char in "+-*/.":
+            self.update_entry(char)
+
     def button_pressed(self, char) -> None:
         if char == "=":
             self.calculate_result()
         else:
             self.update_entry(char)
-    
-    def key_pressed(self, event) -> None:
-        char = event.char
-        if char == "\r":
-            self.calculate_result()
-        else:
-            if self.entry != self.root.focus_get():
-                if char == "\b":
-                    self.entry.delete(len(self.entry.get())-1, tk.END)
-                elif char.isdigit() or char in "+-*/.":
-                    self.update_entry(char)
 
     def update_entry(self, text) -> None:
-        self.current_input = self.entry.get()
         self.current_input += text
         self.entry.delete(0, tk.END)
         self.entry.insert(tk.END, self.current_input)
+
+    def backspace(self) -> None:
+        self.current_input = self.current_input[:-1]
+        self.entry.delete(0, tk.END)
+        self.entry.insert(tk.END, self.current_input)
         
-
     def calculate_result(self) -> None:     
-        user_input = self.entry.get()
-        if user_input == "1234":
-            self.activate_easter_egg()
-        else:
-            try:
-                solution = eval(user_input)
-                self.entry.delete(0, tk.END)
-                self.entry.insert(tk.END, str(solution))
-            except ZeroDivisionError:
-                self.entry.delete(0, tk.END)
-                self.entry.insert(tk.END, "Division by zero")
-            except Exception as e:
-                self.entry.delete(0, tk.END)
-                self.entry.insert(tk.END, "Error")
-    
-    def activate_easter_egg(self):
-        print("Easter Egg activated")
-
+        user_input = self.current_input
+        try:
+            solution = eval(user_input)
+            self.entry.delete(0, tk.END)
+            self.entry.insert(tk.END, str(solution))
+            self.current_input = str(solution)
+        except ZeroDivisionError:
+            self.entry.delete(0, tk.END)
+            self.entry.insert(tk.END, "Division by zero")
+            self.current_input = ""
+        except Exception as e:
+            self.entry.delete(0, tk.END)
+            self.entry.insert(tk.END, "Error")
+            self.current_input = ""
 
 if __name__ == "__main__":
     root = tk.Tk()
