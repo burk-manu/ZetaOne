a81fc9d91af2de475c485851d65d765de630ae42
Manuel Bürki <burk.manu.2022@ksz.edu-zg.ch>
2025-04-18 10:22:25 +0200

    ui.py new module to initialize ui

---
+++ modules/ui.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+
+def init_buttons(app):
+    buttons = [
+    ("MC", "MR", "M+", "M-", "C"),
+    ("log", "ln", "|x|", "√", "^"),
+    ("sin", "cos", "tan", "(", ")"),
+    ("7", "8", "9", "/", "π"),
+    ("4", "5", "6", "*", "e"),
+    ("1", "2", "3", "-", "="),
+    ("0", ".", "±", "+", "⌫"),
+    ]
+
+    for i, row in enumerate(buttons):
+        for j, text in enumerate(row):
+            button = tk.Button(app.root, text=text, width=3, height=1, font=("Helvetica Neue", 24),
+                            bg="#262626", fg="#C04F15",
+                            activebackground="#595959", activeforeground="#C04F15",
+                            command=lambda t=text: button_pressed(t))
+            button.grid(row=i+1, column=j, padx=5, pady=5, sticky="nsew")
+            app.buttons[text] = button
+
+def button_pressed(app, char) -> None:
+    if char == "=":
+        app.calculate_result()
+    elif char == "C":
+        app.clear_entry()
+    elif char == "⌫":
+        app.backspace()
+    elif char == "±":
+        app.toggle_sign()
+    elif char in ["sin", "cos", "tan"]:
+        app.update_entry(char + "(")
+    else:
+        app.update_entry(char)
