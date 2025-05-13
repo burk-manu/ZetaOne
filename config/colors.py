ab4fa337fd7d18cf099b9e816d757809c5d4f7d7
Manuel Bürki <burk.manu.2022@ksz.edu-zg.ch>
2025-05-13 17:04:06 +0200

    added module config.colors

---
+++ config/colors.py
@@ -0,0 +1,14 @@
+# colors.py
+
+# Background Colors
+BG_BLACK = "#000000"
+BG_DARKGREY = "#262626"
+BG_LIGHTGREY = "#565656"
+
+# Foreground Colors
+FG_ORANGE = "#C04F15"
+FG_WHITE = "#FFFFFF"
+FG_BLUE = "#4C58FF"
+FG_GREEN = "#02FFA6"
+FG_RED = "#FF005D"
+FG_PINK = "#FF28C2"
